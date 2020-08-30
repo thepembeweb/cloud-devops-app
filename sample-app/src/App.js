a72cc1b07c3ac9c0d08d39e12a80d1b7f8cd2981
Pemberai Sweto <pembeweb@yahoo.com>
2020-08-30 21:23:34 +0200

    feat: add sample react app

---
+++ sample-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Cloud DevOps Sample App</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
