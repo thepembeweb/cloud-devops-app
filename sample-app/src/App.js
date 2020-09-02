24d67ce31e11491d16662c1d28ce87b84e3dff17
Pemberai Sweto <pembeweb@yahoo.com>
2020-09-02 15:23:46 +0200

    fix: update tests

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -5,7 +5,7 @@ function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>Cloud DevOps Sample App</p>
+        <p>Cloud DevOps Sample1 App</p>
       </header>
     </div>
   )
